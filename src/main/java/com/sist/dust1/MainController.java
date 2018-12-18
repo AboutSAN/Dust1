798f197c944af27e9fbb86e08a05cde25e5df891
sist <sist@localhost>
2018-12-18 14:12:44 +0900

    DustProject 시작입니다

---
+++ src/main/java/com/sist/dust1/MainController.java
@@ -0,0 +1,19 @@
+package com.sist.dust1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+	@RequestMapping("main.do")
+	public String main_page()
+	{
+		return "main";
+	}
+	
+	@RequestMapping("map.do")
+	public String main_map()
+	{
+		return "map";
+	}
+}
